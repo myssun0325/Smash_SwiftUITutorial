336fc8e14deeb291be0a664951a2cfc514c1574f
ChangMin <sopg04@gmail.com>
2023-05-23 23:17:27 +0900

    feat: Chapter 1

---
+++ sakwa/LandmarksApp/LandmarksApp/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  LandmarksApp
+//
+//  Created by ChangMin on 2023/05/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ sakwa/LandmarksApp/LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by ChangMin on 2023/05/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
