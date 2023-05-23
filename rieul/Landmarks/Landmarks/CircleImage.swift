1efba74e5e6b9aa57c36efb3b4f126a1f82e3ebe
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-23 22:19:24 +0900

    Create a Custom Image View

---
+++ rieul/Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by rieul on 2023-05-23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle()
+                    .stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
