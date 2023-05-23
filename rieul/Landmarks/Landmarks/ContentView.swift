6323c1a695f41bf6f65d55bcaab408a09eb5ed20
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-23 21:34:14 +0900

    Customize the Text View

--- rieul/Landmarks/Landmarks/ContentView.swift
+++ rieul/Landmarks/Landmarks/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello SwiftUI!")
-            .padding()
+        Text("Turtle Rock")
+            .font(.title)
     }
 }
 
