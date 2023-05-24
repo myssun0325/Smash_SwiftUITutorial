ef73b47231a56c2b961d7dbbac5f28801c708077
ChangMin <sopg04@gmail.com>
2023-05-24 23:35:21 +0900

    feat: Chapter 1 Finish

--- sakwa/LandmarksApp/LandmarksApp/Views/Helpers/CircleImage.swift
+++ sakwa/LandmarksApp/LandmarksApp/Views/Helpers/CircleImage.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image("turtlerock")
+        image
             .clipShape(Circle())
             .overlay {
                 Circle().stroke(.white, lineWidth: 4)
@@ -20,6 +22,6 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
---
+++ sakwa/LandmarksApp/LandmarksApp/Views/Landmarks/LandmarkRow.swift
@@ -0,0 +1,41 @@
+//
+//  LandmarkRow.swift
+//  LandmarksApp
+//
+//  Created by ChangMin on 2023/05/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            
+            Text(landmark.name)
+            
+            Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
