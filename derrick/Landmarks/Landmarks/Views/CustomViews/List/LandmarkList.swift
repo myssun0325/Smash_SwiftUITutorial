846dd05696feadff7bddae6e44f1ae5f7251f107
derrickkim0109 <derrickkim0109@naver.com>
2023-05-26 10:17:43 +0900

    feat: Dynamic한 List 생성

--- derrick/Landmarks/Landmarks/Views/CustomViews/List/LandmarkList.swift
+++ derrick/Landmarks/Landmarks/Views/CustomViews/List/LandmarkList.swift
@@ -9,9 +9,8 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List {
-            LandmarkRow(landmark: landmarks[0])
-            LandmarkRow(landmark: landmarks[1])
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
         }
     }
 }
