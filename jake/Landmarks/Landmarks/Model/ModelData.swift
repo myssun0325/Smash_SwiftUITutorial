2ea1e3f4c643b508329340135e33c47a70943221
Taehyeon Jake Lee <nayalth@gmail.com>
2023-05-27 00:15:54 +0900

    🎉 [Chapter 2-2] Animating Views and Transitions

--- jake/Landmarks/Landmarks/Model/ModelData.swift
+++ jake/Landmarks/Landmarks/Model/ModelData.swift
@@ -10,6 +10,7 @@ import Combine
 
 final class ModelData: ObservableObject { /* SwiftUI가 subscribe 하는 객체. 해당 객체가 변경되면 어떤 뷰던지 refreshing이 필요하면 갱신된다. */
     @Published var landmarks: [Landmark] = load("landmarkData.json") /* subscriber가 변화를 감지할 수 있도록 data 변화를 publishing 할 수 있도록 함 */
+    var hikes: [Hike] = load("hikeData.json")
 }
 
 
