8ac8ef9acff5c229d96507ba5de00a23c48e810f
derrickkim0109 <derrickkim0109@naver.com>
2023-05-26 09:26:58 +0900

    feat: Section 4 커스텀 이미지 뷰 생성

---
+++ derrick/Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Derrick kim on 2023/05/26.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
