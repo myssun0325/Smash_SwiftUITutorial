a0be62bde4a7200584eb030f2d11371b1e522669
derrickkim0109 <derrickkim0109@naver.com>
2023-05-26 10:22:48 +0900

    feat: Navigation 생성
    - List에서 DetailView로 이동하는 기능

--- derrick/Landmarks/Landmarks/Views/ContentView.swift
+++ derrick/Landmarks/Landmarks/Views/ContentView.swift
@@ -9,40 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
                 }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-
-                Divider()
-
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
             }
-            .padding()
-
-            Spacer()
+            .navigationTitle("Landmarks")
         }
+
     }
 }
 
