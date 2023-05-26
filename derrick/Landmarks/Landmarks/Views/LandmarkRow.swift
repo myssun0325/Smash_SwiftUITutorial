c709e60a8ee3b4e0711aa9d947b41ea9bf0cdf6f
derrickkim0109 <derrickkim0109@naver.com>
2023-05-26 10:08:12 +0900

    feat: Section 2 Row View 생성

---
+++ derrick/Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Derrick kim on 2023/05/26.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
