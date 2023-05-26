7374c5b0c2a3187881cd32f2c6578f086057582c
derrickkim0109 <derrickkim0109@naver.com>
2023-05-26 09:17:21 +0900

    add: 프로젝트 생성

---
+++ derrick/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Derrick kim on 2023/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
