dba28563986ad1f30313a5c6bd464e613b2efa89
derrickkim0109 <derrickkim0109@naver.com>
2023-05-26 10:15:54 +0900

    feat: Landmarks List 생성

---
+++ derrick/Landmarks/Landmarks/Views/CustomViews/List/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Derrick kim on 2023/05/26.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
