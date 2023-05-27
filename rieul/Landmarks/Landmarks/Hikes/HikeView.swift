3b69304394135289282dbd2ddb0ec658662ba3aa
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-27 15:41:47 +0900

    Customize View Transitions

--- rieul/Landmarks/Landmarks/Hikes/HikeView.swift
+++ rieul/Landmarks/Landmarks/Hikes/HikeView.swift
@@ -7,6 +7,15 @@ A view displaying information about a hike, including an elevation graph.
 
 import SwiftUI
 
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        .asymmetric(
+            insertion: .move(edge: .trailing).combined(with: .opacity),
+            removal: .scale.combined(with: .opacity)
+        )
+    }
+}
+
 struct HikeView: View {
     var hike: Hike
     @State private var showDetail = false
@@ -41,6 +50,7 @@ struct HikeView: View {
 
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.moveAndFade)
             }
         }
     }
