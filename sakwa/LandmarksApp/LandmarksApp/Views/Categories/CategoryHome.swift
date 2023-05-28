162a0e9dddd8ebb515e75e4caa7c923119bb6bdb
ChangMin <sopg04@gmail.com>
2023-05-28 17:41:49 +0900

    feat: Chapter 4 - Interfacing with UIKit

--- sakwa/LandmarksApp/LandmarksApp/Views/Categories/CategoryHome.swift
+++ sakwa/LandmarksApp/LandmarksApp/Views/Categories/CategoryHome.swift
@@ -14,12 +14,12 @@ struct CategoryHome: View {
     var body: some View {
         NavigationStack {
             List {
-                modelData.features[0].image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 250)
-                    .clipped()
-                    .listRowInsets(EdgeInsets())
+                PageView(
+                    pages: modelData.features.map { FeatureCard(landmark: $0)
+                    }
+                )
+                .aspectRatio(3/2, contentMode: .fit)
+                .listRowInsets(EdgeInsets())
                 
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(
