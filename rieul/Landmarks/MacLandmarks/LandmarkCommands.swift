025e4df2d9cf888c0f42e8cdbcaa94e3e37b6af7
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-29 22:09:00 +0900

    Add a Custom Menu Command

--- rieul/Landmarks/MacLandmarks/LandmarkCommands.swift
+++ rieul/Landmarks/MacLandmarks/LandmarkCommands.swift
@@ -9,7 +9,27 @@
 import SwiftUI
 
 struct LandmarkCommands: Commands {
+    @FocusedBinding(\.selectedLandmark) var selectedLandmark
+    
     var body: some Commands {
         SidebarCommands()
+        
+        CommandMenu("Landmark") {
+            Button("\(selectedLandmark?.isFavorite == true ? "Remove" : "Mark") as Favorite") {
+                selectedLandmark?.isFavorite.toggle()
+            }
+            .disabled(selectedLandmark == nil)
+        }
+    }
+}
+
+private struct SelectedLandmarkKey: FocusedValueKey {
+    typealias Value = Binding<Landmark>
+}
+
+extension FocusedValues {
+    var selectedLandmark: Binding<Landmark>? {
+        get { self[SelectedLandmarkKey.self] }
+        set { self[SelectedLandmarkKey.self] = newValue }
     }
 }
