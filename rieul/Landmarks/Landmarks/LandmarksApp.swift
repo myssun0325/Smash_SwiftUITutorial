f5b53fc8a600a6ca22f1d8be91b3f088f94717d6
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-29 21:08:30 +0900

    Create a Custom Notification Interface

--- rieul/Landmarks/Landmarks/LandmarksApp.swift
+++ rieul/Landmarks/Landmarks/LandmarksApp.swift
@@ -16,5 +16,9 @@ struct LandmarksApp: App {
             ContentView()
                 .environmentObject(modelData)
         }
+        
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
     }
 }
