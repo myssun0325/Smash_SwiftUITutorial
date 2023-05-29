d82866a5bfb2a7122e89c14d5dc21aa5a17f751d
napster <comets195@gmail.com>
2023-05-29 21:25:08 +0900

    Chapter1 Handling User Input

--- napster/Landmarks/Landmarks/Model/ModelData.swift
+++ napster/Landmarks/Landmarks/Model/ModelData.swift
@@ -6,8 +6,12 @@
 //
 
 import Foundation
+import Combine
 
-var landmarks: [Landmark] = load("landmarkData.json")
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
+
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
--- napster/Landmarks/Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ napster/Landmarks/Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
     
     var body: some View {
         ScrollView {
@@ -44,7 +50,10 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
+        LandmarkDetail(landmark: ModelData().landmarks[0])
+            .environmentObject(modelData)
     }
 }
