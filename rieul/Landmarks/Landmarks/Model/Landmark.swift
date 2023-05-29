c3e10d9bf841273252b714854c80bc1c7ab69629
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-29 15:20:29 +0900

    Complete the Category View

--- rieul/Landmarks/Landmarks/Model/Landmark.swift
+++ rieul/Landmarks/Landmarks/Model/Landmark.swift
@@ -17,6 +17,7 @@ struct Landmark: Hashable, Codable, Identifiable {
     var state: String
     var description: String
     var isFavorite: Bool
+    var isFeatured: Bool
     
     var category: Category
     enum Category: String, CaseIterable, Codable {
