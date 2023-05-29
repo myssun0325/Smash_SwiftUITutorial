54303505fba80917b890eeda6fc883aced8ffe5a
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-29 20:22:45 +0900

    Add a watchOS Target

---
+++ rieul/Landmarks/WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by rieul on 2023-05-29.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rieul/Landmarks/WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by rieul on 2023-05-29.
+//  
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
