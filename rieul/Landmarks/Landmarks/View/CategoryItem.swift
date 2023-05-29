380ada53abf618c0543ca93eabdbe27d6f3ba724
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-29 15:14:46 +0900

    Create a Category Row

---
+++ rieul/Landmarks/Landmarks/View/CategoryItem.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by rieul on 2023-05-29.
+//  
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
