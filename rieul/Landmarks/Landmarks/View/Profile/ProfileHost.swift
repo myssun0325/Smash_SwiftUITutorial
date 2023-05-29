f51f245670b4e95bb5a1f9b4d72ae2ce8336664d
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-29 16:12:42 +0900

    Define the Profile Editor

--- rieul/Landmarks/Landmarks/View/Profile/ProfileHost.swift
+++ rieul/Landmarks/Landmarks/View/Profile/ProfileHost.swift
@@ -23,7 +23,7 @@ struct ProfileHost: View {
             if editMode?.wrappedValue == .inactive {
                 ProfileSummary(profile: modelData.profile)
             } else {
-                Text("Profile Editor")
+                ProfileEditor(profile: $draftProfile)
             }
         }
         .padding()
