12e419d76aa6ba9c1fc51078fad93150192771da
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-29 21:14:11 +0900

    Add a macOS Target to the Project

---
+++ rieul/Landmarks/MacLandmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by rieul on 2023-05-29.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
