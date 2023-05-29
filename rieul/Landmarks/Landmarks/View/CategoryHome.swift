98544c52aaeda09179198044b903bf84927d2631
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-29 15:04:39 +0900

    Add a Category View

---
+++ rieul/Landmarks/Landmarks/View/CategoryHome.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by rieul on 2023-05-29.
+//  
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
