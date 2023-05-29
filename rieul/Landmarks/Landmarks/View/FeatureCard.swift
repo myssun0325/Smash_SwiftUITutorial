a892c2ed6e3a5b84928cccd915e5da06efa204cc
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-29 19:56:01 +0900

    Create the View Controller’s Data Source

--- rieul/Landmarks/Landmarks/View/FeatureCard.swift
+++ rieul/Landmarks/Landmarks/View/FeatureCard.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct FeatureCard: View {
     var landmark: Landmark
-    
+
     var body: some View {
         landmark.featureImage?
             .resizable()
@@ -23,13 +23,13 @@ struct FeatureCard: View {
 
 struct TextOverlay: View {
     var landmark: Landmark
-    
+
     var gradient: LinearGradient {
         .linearGradient(
             Gradient(colors: [.black.opacity(0.6), .black.opacity(0)]),
             startPoint: .bottom,
             endPoint: .center
-        )
+        )
     }
 
     var body: some View {
--- rieul/Landmarks/Landmarks/View/PageView/PageViewController.swift
+++ rieul/Landmarks/Landmarks/View/PageView/PageViewController.swift
@@ -12,10 +12,16 @@ import UIKit
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
-            navigationOrientation: .horizontal)
+            navigationOrientation: .horizontal
+        )
+        pageViewController.dataSource = context.coordinator
         
         return pageViewController
     }
@@ -25,9 +31,45 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
         context: Context
     ) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])],
+            [context.coordinator.controllers[0]],
             direction: .forward,
             animated: true
         )
+    }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        var controllers: [UIViewController] = []
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController
+        ) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return controllers.last
+            }
+            return controllers[index - 1]
+        }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController
+        ) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            return controllers[index + 1]
+        }
     }
 }
