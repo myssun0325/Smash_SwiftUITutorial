f29e2cf41cdb6d0af912ae64b5813e6e7e4418d4
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-29 15:08:11 +0900

    Create a Category List

--- rieul/Landmarks/Landmarks/Model/ModelData.swift
+++ rieul/Landmarks/Landmarks/Model/ModelData.swift
@@ -12,6 +12,13 @@ import Foundation
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
