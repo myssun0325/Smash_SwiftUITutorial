18520aedeeac15b02087137af4cc124973736ae1
JiWon Lee <jiwon.swdev@gmail.com>
2023-05-29 16:06:15 +0900

    Add an Edit Mode

--- rieul/Landmarks/Landmarks/Model/ModelData.swift
+++ rieul/Landmarks/Landmarks/Model/ModelData.swift
@@ -12,6 +12,7 @@ import Foundation
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    @Published var profile: Profile = .default
     
     var features: [Landmark] {
         landmarks.filter { $0.isFeatured }
--- rieul/Landmarks/Landmarks/View/Profile/ProfileHost.swift
+++ rieul/Landmarks/Landmarks/View/Profile/ProfileHost.swift
@@ -9,11 +9,22 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
     @State private var draftProfile: Profile = .default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: draftProfile)
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                Text("Profile Editor")
+            }
         }
         .padding()
     }
@@ -22,5 +33,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
