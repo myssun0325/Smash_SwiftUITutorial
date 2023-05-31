13878b1c0165a49f951dde52ff1b41e6eff45628
derrickkim0109 <derrickkim0109@naver.com>
2023-05-31 13:38:13 +0900

    feat: [Chapter 4] Creating a watchOS App

--- derrick/Landmarks/Landmarks/LandmarksApp.swift
+++ derrick/Landmarks/Landmarks/LandmarksApp.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 @main
 struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
+
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
     }
 }
---
+++ derrick/Landmarks/WatchLanmarks Watch App/NotificationView.swift
@@ -0,0 +1,43 @@
+//
+//  NotificationView.swift
+//  WatchLanmarks Watch App
+//
+//  Created by Derrick kim on 2023/05/31.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var title: String?
+    var message: String?
+    var landmark: Landmark?
+
+    var body: some View {
+        VStack {
+            if landmark != nil {
+                CircleImage(image: landmark!.image.resizable())
+                    .scaledToFit()
+            }
+
+            Text(title ?? "Unknown Landmark")
+                .font(.headline)
+
+            Divider()
+
+            Text(message ?? "You are within 5 miles of one of your favorite landmarks.")
+                .font(.caption)
+        }
+        .lineLimit(0)
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            NotificationView()
+            NotificationView(title: "Turtle Rock",
+                             message: "You are within 5 miles of Turtle Rock.",
+                             landmark: ModelData().landmarks[0])
+        }
+    }
+}
