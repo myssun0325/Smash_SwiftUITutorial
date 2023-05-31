acd37fea68108958523c3aceb440068043d07aa8
derrickkim0109 <derrickkim0109@naver.com>
2023-05-31 13:24:35 +0900

    feat: [Chapter 4] Interfacing with UIKit

---
+++ derrick/Landmarks/Landmarks/Views/CustomViews/PageView/PageView.swift
@@ -0,0 +1,29 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Derrick kim on 2023/05/31.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
--- derrick/Landmarks/Landmarks/Views/CustomViews/Profile/ProfileHost.swift
+++ derrick/Landmarks/Landmarks/Views/CustomViews/Profile/ProfileHost.swift
@@ -11,7 +11,7 @@ struct ProfileHost: View {
     @Environment(\.editMode) var editMode
     @EnvironmentObject var modelData: ModelData
     @State private var draftProfile = Profile.default
-    
+
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack {
@@ -21,7 +21,7 @@ struct ProfileHost: View {
                         editMode?.animation().wrappedValue = .inactive
                     }
                 }
-                
+
                 Spacer()
                 EditButton()
             }
