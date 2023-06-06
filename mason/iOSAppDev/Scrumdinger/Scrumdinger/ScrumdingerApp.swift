c0e08970d79cc20fedeff52dc05a8457c295bced
mason <myssun0325@gmail.com>
2023-06-06 23:27:34 +0900

    AppDev - SwiftUI essentials

---
+++ mason/iOSAppDev/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Mason on 2023/06/06.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
---
+++ mason/iOSAppDev/UsingStacksToArrangeViews/Complete/README.md
@@ -0,0 +1,5 @@
+# Using Stacks to Arrange Views
+
+## Completed Project
+
+Explore the completed project for [Using Stacks to Arrange Views](https://developer.apple.com/tutorials/app-dev-training/using-stacks-to-arrange-views).
