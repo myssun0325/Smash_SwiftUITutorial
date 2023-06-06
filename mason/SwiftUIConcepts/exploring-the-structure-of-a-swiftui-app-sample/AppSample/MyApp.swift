cc119f5c4008bf2e7bc37d5c70c35914961957dd
mason <myssun0325@gmail.com>
2023-06-06 21:56:48 +0900

    SwiftUI Conecepts Tutorials ch.1

---
+++ mason/SwiftUIConcepts/exploring-the-structure-of-a-swiftui-app-sample/AppSample/MyApp.swift
@@ -0,0 +1,14 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
