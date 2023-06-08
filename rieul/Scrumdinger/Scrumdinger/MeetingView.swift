30265a55570b7ca4e06c45a417a5da0a9b649ecb
JiWon Lee <jiwon.swdev@gmail.com>
2023-06-08 23:04:35 +0900

    Create a project

---
+++ rieul/Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by rieul on 2023-06-06.
+//  
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
---
+++ rieul/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by rieul on 2023-06-06.
+//  
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
