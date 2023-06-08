e83006321b6e7d9152f0f0bfa3123f93bdcb3f92
JiWon Lee <jiwon.swdev@gmail.com>
2023-06-08 23:11:13 +0900

    Supplement accessibility data

--- rieul/Scrumdinger/Scrumdinger/MeetingView.swift
+++ rieul/Scrumdinger/Scrumdinger/MeetingView.swift
@@ -25,6 +25,9 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
             Circle()
                 .strokeBorder(lineWidth: 24)
             HStack {
@@ -33,6 +36,7 @@ struct MeetingView: View {
                 Button(action: {}) {
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
