799c94893e41ba09c801fcb566c28c7909823589
Taehyeon Jake Lee <nayalth@gmail.com>
2023-06-08 23:42:40 +0900

    🎉 Using stacks to arrange views

---
+++ jake/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by taehyeon.lee on 2023/06/08.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
