910b2cba7110129aa32cb6070d3e56650ab99ebd
ChangMin <sopg04@gmail.com>
2023-06-12 23:21:19 +0900

    [iOS App Dev] Ch. 2 Creating a card view

---
+++ sakwa/iOS App Dev Tutorials/Scrumdinger/Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,39 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by ChangMin on 2023/06/12.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] = [
+        DailyScrum(
+            title: "Design",
+            attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+            lengthInMinutes: 10,
+            theme: .yellow
+        ),
+        DailyScrum(
+            title: "App Dev",
+            attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+            lengthInMinutes: 5,
+            theme: .orange
+        ),
+        DailyScrum(
+            title: "Web Dev",
+            attendees: ["Chella", "Chris", "Christina", "Eden", "Karla",
+                        "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+            lengthInMinutes: 5,
+            theme: .poppy
+        ),
+    ]
+}
---
+++ sakwa/iOS App Dev Tutorials/Scrumdinger/Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by ChangMin on 2023/06/12.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
