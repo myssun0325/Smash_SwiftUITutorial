2d585fb96a6df2449a804722c320d0cab0bb2ef6
JiWon Lee <jiwon.swdev@gmail.com>
2023-06-17 16:41:27 +0900

    Create the card view

---
+++ rieul/Scrumdinger/Scrumdinger/CardView.swift
@@ -0,0 +1,33 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by rieul on 2023-06-17.
+//  
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+            }
+        }
+    }
+}
+
+#Preview {
+    var scrum = DailyScrum.sampleData[0]
+
+    return CardView(scrum: scrum)
+        .background(scrum.theme.mainColor)
+        .previewLayout(.fixed(width: 400, height: 60))
+}
